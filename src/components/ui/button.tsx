c4d52a90548860d58f5eaa5f6ad57ad7a8a1ef30
邓超文 <dengchaowen@minieye.cc>
2026-09-14 17:08:45 +0800

    feat: 调整按钮颜色

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,10 +8,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow-sm hover:bg-primary/90',
+        default: 'bg-white text-slate-950 shadow-sm hover:bg-blue-200',
         destructive: 'bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90',
-        outline: 'border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
+        outline: 'border border-input bg-white text-slate-950 shadow-xs hover:bg-blue-200 hover:text-slate-950',
+        secondary: 'bg-white text-slate-950 shadow-xs hover:bg-blue-200',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
