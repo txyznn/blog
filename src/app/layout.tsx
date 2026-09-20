5c49ae2b2b9186a0e0ebb7c1937536c63d30af4d
邓超文 <dengchaowen@minieye.cc>
2026-09-20 14:32:33 +0800

    fix: 修复主题切换 hydration 警告

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
